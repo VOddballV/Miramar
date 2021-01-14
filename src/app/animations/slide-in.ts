fb638296562ff78f0dec8aadb4d7ee3d1fa267a6
Unknown <oddballbdd@hotmail.com>
2021-01-13 21:23:36 -0500

    Only need to trigger when coming from the void

--- src/app/animations/slide-in.ts
+++ src/app/animations/slide-in.ts
@@ -1,12 +1,7 @@
-import { trigger, transition, style, animate, sequence, query, stagger, keyframes } from '@angular/animations';
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
 export const listAnimation = trigger('listAnimation', [
-	transition('* => *', [ // each time the binding value changes
-	  query(':leave', [
-		stagger(100, [
-		  animate('0.5s', style({ opacity: 0 }))
-		])
-	  ], { optional: true }),
+	transition('void => *', [ // each time the binding value changes
 	  query(':enter', [
 		style({ opacity: 0 }),
 		stagger(100, [
